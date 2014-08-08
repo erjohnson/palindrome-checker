56bd2160b0ee324078e0379229f8eee3f8584700
Eric <erjohnson.pdx@gmail.com>
2014-08-07 21:28:21 -0700

    Add failing (non-palindrome) test

--- spec/palindrome_check_spec.rb
+++ spec/palindrome_check_spec.rb
@@ -13,4 +13,8 @@ describe 'palindrome_check' do
   it 'ignores special characters' do
     expect(palindrome_check('?race, car:!')).to eq true
   end
+
+  it 'returns false if not a palindrome' do
+    expect(palindrome_check('cathy jumps')).to eq false
+  end
 end
